844160027fa38dc097695c510e3aa74d2640ada9
quangthanh010290 <thanh@autonomous.nyc>
2017-05-07 11:19:25 -0700

    demo question

--- examples/basic/buildingResponse.py
+++ examples/basic/buildingResponse.py
@@ -14,12 +14,21 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
+@ask.launch
+def fcn():
+    return question("What is your name?")
+
 
 @ask.intent("MyNameIsIntent")
 
 def name_fcn(firstname):
     msg = "Your name is {}".format(firstname)
-    return statement(msg).simple_card("Hello {}".format(firstname),msg)
+
+    return statement(msg).simple_card("Hello {}". format(firstname) ,msg) 
+
+@ask.intent("WhatIsMyNameIntent")
+def fcn():
+    return question("What is your name?")
 if __name__ == '__main__':
 
     app.run(debug=True)
