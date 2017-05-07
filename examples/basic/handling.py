d0651e01350592bfae365a60ec4d624b6dbf59bb
quangthanh010290 <thanh@autonomous.nyc>
2017-05-07 10:18:44 -0700

    intent request

--- examples/basic/handling.py
+++ examples/basic/handling.py
@@ -20,6 +20,13 @@ def launch():
 
     return statement("Welcome to the request demo, this is launch request")
 
+@ask.intent("HelloIntent")
+
+def intent_fcn():
+    return statement("This is handler of hello intent")
+
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
