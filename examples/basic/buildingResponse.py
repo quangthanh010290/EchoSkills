b5703134ea25c42a4104ddc63630bd6af133f838
quangthanh010290 <thanh@autonomous.nyc>
2017-05-07 11:08:12 -0700

    response

---
+++ examples/basic/buildingResponse.py
@@ -0,0 +1,25 @@
+import logging
+
+from random import randint
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, statement, question, session
+
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+
+@ask.intent("MyNameIsIntent")
+
+def name_fcn(firstname):
+    msg = "Your name is {}".format(firstname)
+    return statement(msg)
+if __name__ == '__main__':
+
+    app.run(debug=True)
