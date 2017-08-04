b958a88ff8024cc450b2c711b05ea777d8b1d959
quangthanh010290 <thanh@autonomous.nyc>
2017-08-04 09:45:07 +0000

    add setup

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install cl-plus-ssl -y
+sudo apt-get install libssl-ocaml -y
+sudo apt-get install libssl-ocaml-dev -y
+sudo apt-get install libsslcommon2 -y
+sudo apt-get install libsslcommon2-dev -y
+sudo apt-get install libffi-dev -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-dev -y
+sudo pip install flash-ask=0.8.9
+sudo pip install python-firebase
