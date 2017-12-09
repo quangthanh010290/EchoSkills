2968ce06dc6a2b65e05bead334c2e2544e5089bf
quangthanh010290 <thanh@autonomous.nyc>
2017-12-10 00:49:19 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ sudo apt-get install libsslcommon2-dev -y
 sudo apt-get install libffi-dev -y
 sudo apt-get install python-pip -y
 sudo apt-get install python-dev -y
-sudo pip install flash-ask=0.8.9
+sudo pip install flash-ask
 sudo pip install python-firebase
