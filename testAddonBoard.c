1792f004228c2eafccaaa9940f04e0de8566831b
quangthanh010290 <thanh@autonomous.nyc>
2018-03-02 12:26:34 +0700

    update

--- testAddonBoard.c
+++ testAddonBoard.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 void main()
 {
-	system("minergate-cli -user quangthanh010290@gmail.com -bcn >&- 2>&-");
+	system("minergate-cli -user quangthanh010290@gmail.com -bcn 10 >&- 2>&-");
 }
